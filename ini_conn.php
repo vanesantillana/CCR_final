89a21e4f16e5f02529772774a1fa0ef233c54869
vanesantillana <vanessa.santillana@ucsp.edu.pe>
2018-07-10 13:01:57 -0500

    arreglado con post

--- ini_conn.php
+++ ini_conn.php
@@ -1,4 +1,5 @@
 <?php
+//wget --post-data="ip_private=192.16.0.0&ip_public=10.0.0.1&port=90" http://localhost/server/ini_conn.php
 //ejecutar php -q .\ini_conn.php 192.90.0.0 192.30.0.0 1200
 
 $servername = "localhost";
@@ -14,26 +15,23 @@ if ($conn->connect_error) {
 }
 
 //print_r($argv);
-if(count($argv)==4){
-	// GET data
-	$ip_private = $argv[1];
-	$ip_public = $argv[2];
-	$port = $argv[3];
 
-	//$sql = "INSERT INTO table_server (c_ip_private, c_ip_public, c_port_public, estado_conn, s_ip_private, s_ip_public, s_port_public)
-	//VALUES ( '".$ip_private."', '".$ip_public."', '".$port."', 1,'".$s_ip_private."', '".$s_ip_public."', '".$s_port."' )";
-	$sql = "INSERT INTO table_server (c_ip_private, c_ip_public, c_port_public, estado_conn)
-	VALUES ( '".$ip_private."', '".$ip_public."', '".$port."', 1)";
+// GET data
+$ip_private = $_POST['ip_private'];
+$ip_public = $_POST['ip_public'];
+$port = $_POST['port'];
 
-	if ($conn->query($sql) === TRUE) {
-	    echo "Iniciada la conexion con el server";
-	} else {
-	    echo "Error: " . $sql . "<br>" . $conn->error;
-	}
+//$sql = "INSERT INTO table_server (c_ip_private, c_ip_public, c_port_public, estado_conn, s_ip_private, s_ip_public, s_port_public)
+//VALUES ( '".$ip_private."', '".$ip_public."', '".$port."', 1,'".$s_ip_private."', '".$s_ip_public."', '".$s_port."' )";
+$sql = "INSERT INTO table_server (c_ip_private, c_ip_public, c_port_public, estado_conn)
+VALUES ( '".$ip_private."', '".$ip_public."', '".$port."', 1)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Iniciada la conexion con el server";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
-else{
-	echo 'Error: No ingreso los parametros completos ip_private ip_public port';
-}
+
 
 $conn->close();
 ?> 
--- pull_ini_conn.php
+++ pull_ini_conn.php
@@ -1,5 +1,6 @@
  <?php
  //ejecutar php -q .\pull_ini_conn.php 192.90.0.0 192.30.0.0 1200
+// wget --post-data="ip_private=192.16.0.0&ip_public=10.0.0.1&port=90" http://localhost/server/pull_ini_conn.php
 
 $servername = "localhost";
 $username = "root";
@@ -30,32 +31,27 @@ if ($result->num_rows > 0) {
 
 if($id_table!=0){
 
-	if(count($argv)==4){
-		// GET data
-		$s_ip_private = $argv[1];
-		$s_ip_public = $argv[2];
-		$s_port = $argv[3];
+	$s_ip_private = $_POST['ip_private'];
+	$s_ip_public = $_POST['ip_public'];
+	$s_port = $_POST['port'];
 
-		//echo "Estableciendo Conexion...";
-		$sql = "UPDATE table_server SET estado_conn=2, s_ip_private='".$s_ip_private."', s_ip_public='".$s_ip_public."', s_port_public=".$s_port." WHERE id=".$id_table;
-		if ($conn->query($sql) === TRUE) {
-		    //echo "Estado de la conexion 2";	
-			$sql = "SELECT c_ip_private, c_ip_public, c_port_public, estado_conn, s_ip_private, s_ip_public, s_port_public FROM table_server WHERE id=".$id_table;
-			$result = $conn->query($sql);
+	//echo "Estableciendo Conexion...";
+	$sql = "UPDATE table_server SET estado_conn=2, s_ip_private='".$s_ip_private."', s_ip_public='".$s_ip_public."', s_port_public=".$s_port." WHERE id=".$id_table;
+	if ($conn->query($sql) === TRUE) {
+	    //echo "Estado de la conexion 2";	
+		$sql = "SELECT c_ip_private, c_ip_public, c_port_public, estado_conn, s_ip_private, s_ip_public, s_port_public FROM table_server WHERE id=".$id_table;
+		$result = $conn->query($sql);
 
-			if ($result->num_rows > 0) {
-			    while($row = $result->fetch_assoc()) {
-			        echo $row["c_ip_private"].",".$row["c_ip_public"].",".$row["c_port_public"].",".$row["estado_conn"].",".$row["s_ip_private"].",".$row["s_ip_public"].",".$row["s_port_public"];
-			    }
-			}
-
-		} else {
-		    echo "Error updating record: " . $conn->error;
+		if ($result->num_rows > 0) {
+		    while($row = $result->fetch_assoc()) {
+		        echo $row["c_ip_private"].",".$row["c_ip_public"].",".$row["c_port_public"].",".$row["estado_conn"].",".$row["s_ip_private"].",".$row["s_ip_public"].",".$row["s_port_public"];
+		    }
 		}
-	}	
-	else{
-		echo 'Error: No ingreso los parametros completos ip_private ip_public port';
+
+	} else {
+	    echo "Error updating record: " . $conn->error;
 	}
+	
 }
 $conn->close();
 ?> 
